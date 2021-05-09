b7c3c3b8b5693cfd56457aec2bbbe73bc6c85572
edimar42 <rodriguesjunior.edimar@gmail.com>
2021-05-09 03:34:14 -0400

    Implementando a aplicação no Windows Forms

---
+++ AppUploadArquivos/AppUploadArquivos/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace AppUploadArquivos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn = new System.Windows.Forms.Button();
+            this.lblMsg1 = new System.Windows.Forms.Label();
+            this.lblMsg2 = new System.Windows.Forms.Label();
+            this.lblMsg3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn
+            // 
+            this.btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn.Location = new System.Drawing.Point(226, 33);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(170, 42);
+            this.btn.TabIndex = 0;
+            this.btn.Text = "Upload";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // lblMsg1
+            // 
+            this.lblMsg1.AutoSize = true;
+            this.lblMsg1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMsg1.Location = new System.Drawing.Point(12, 118);
+            this.lblMsg1.Name = "lblMsg1";
+            this.lblMsg1.Size = new System.Drawing.Size(86, 16);
+            this.lblMsg1.TabIndex = 1;
+            this.lblMsg1.Text = "Mensagem 1";
+            this.lblMsg1.Visible = false;
+            // 
+            // lblMsg2
+            // 
+            this.lblMsg2.AutoSize = true;
+            this.lblMsg2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMsg2.Location = new System.Drawing.Point(12, 169);
+            this.lblMsg2.Name = "lblMsg2";
+            this.lblMsg2.Size = new System.Drawing.Size(86, 16);
+            this.lblMsg2.TabIndex = 2;
+            this.lblMsg2.Text = "Mensagem 2";
+            this.lblMsg2.Visible = false;
+            // 
+            // lblMsg3
+            // 
+            this.lblMsg3.AutoSize = true;
+            this.lblMsg3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMsg3.Location = new System.Drawing.Point(12, 221);
+            this.lblMsg3.Name = "lblMsg3";
+            this.lblMsg3.Size = new System.Drawing.Size(86, 16);
+            this.lblMsg3.TabIndex = 3;
+            this.lblMsg3.Text = "Mensagem 3";
+            this.lblMsg3.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(595, 307);
+            this.Controls.Add(this.lblMsg3);
+            this.Controls.Add(this.lblMsg2);
+            this.Controls.Add(this.lblMsg1);
+            this.Controls.Add(this.btn);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Upload de Arquivos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.Label lblMsg1;
+        private System.Windows.Forms.Label lblMsg2;
+        private System.Windows.Forms.Label lblMsg3;
+    }
+}
+
---
+++ AppUploadArquivos/AppUploadArquivos/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace AppUploadArquivos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+
+            openFile.ShowDialog();
+
+            localhost.WebServiceSoapClient service = new localhost.WebServiceSoapClient();
+
+            System.IO.FileStream fileStream = System.IO.File.Open(openFile.FileName, System.IO.FileMode.Open, System.IO.FileAccess.Read);
+
+            byte[] arquivoByte = new byte[fileStream.Length];
+
+            fileStream.Read(arquivoByte, 0, Convert.ToInt32(fileStream.Length));
+
+            fileStream.Close();
+
+            String resultado = service.UploadArquivo(openFile.SafeFileName, arquivoByte);
+
+            MessageBox.Show(resultado);
+
+            lblMsg1.Text = $"Nome do Arquivo: {openFile.SafeFileName}";
+            lblMsg1.Visible = true;
+
+            if (arquivoByte.Length <= 999)
+            {
+                lblMsg2.Text = $"Tamanho do Arquivo: {arquivoByte.Length}B";
+                lblMsg2.Visible = true;
+            }
+            else if (arquivoByte.Length <= 999999)
+            {
+                lblMsg2.Text = $"Tamanho do Arquivo: {(arquivoByte.Length) / 1000}kB";
+                lblMsg2.Visible = true;
+            }
+            else
+            {
+                lblMsg2.Text = $"Tamanho do Arquivo: {(arquivoByte.Length) / 1000000}MB";
+                lblMsg2.Visible = true;
+            }
+
+            byte[] bytes = Encoding.UTF8.GetBytes(openFile.SafeFileName);
+            SHA256Managed hashstring = new SHA256Managed();
+            byte[] hash = hashstring.ComputeHash(bytes);
+            string hashString = string.Empty;
+            foreach (byte x in hash)
+            {
+                hashString += String.Format("{0:x2}", x);
+            }
+            lblMsg3.Text = $"Código HASH: {hashString}";
+            lblMsg3.Visible = true;
+        }
+    }
+}
